ca51189feeb876789ce4355f2202536465695721
thangduongnfq <thang.duon@nfq.asia>
2022-06-15 11:20:21 +0700

    search-debounce

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { SearchOutlined } from "@ant-design/icons";
+import { Input } from "antd";
+import "./App.css";
+import React, { Component }  from 'react';
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 function App() {
+  const [userData, setUserData] = useState();
+  const [dataSearch, setDataSearch] = useState("");
+  let typingTime = useRef();
+  const debounce = (e) => {
+    clearTimeout(typingTime);
+    typingTime = setTimeout(() => {
+      setDataSearch(e.target.value);
+    }, 1000);
+  };
+  useEffect(() => {
+    const fetchAllData = async (dataSearch) => {
+      const response = await axios.get(
+        `https://hn.algolia.com/api/v1/search?query=${dataSearch}`
+      );
+      dataSearch && setUserData(response.data.hits);
+    };
+    fetchAllData(dataSearch);
+  }, [dataSearch]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="search-field">
+          <Input
+            prefix={<SearchOutlined />}
+            onChange={(e) => {
+              debounce(e);
+            }}
+          />
+          {userData &&
+            userData.map((item) => {
+              if (item.title) return <li>{item.title}</li>;
+            })}
+        </div>
+      </div>
+    </>
   );
 }
 
